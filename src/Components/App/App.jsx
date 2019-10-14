3e11528ff462bef0f97fbad02cb00378cdcf8e11
SvisT <svisteckij@gmail.com>
2019-10-14 15:15:19 +0300

    push

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable import/extensions */
+import React from 'react';
+import styles from './App.module.css';
+
+const App = () => (
+  <div className={styles.App}>
+    <p className="p">Hello!</p>
+  </div>
+);
+
+export default App;
